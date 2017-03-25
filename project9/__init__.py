ef035a5f6a92dea203157daed617b061bd08f5ed
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2017-03-25 18:42:45 -0400

    SQLite and flashed messages!

--- project9/__init__.py
+++ project9/__init__.py
@@ -5,20 +5,21 @@ app = Flask(__name__)
 app.debug = True
 app.secret_key = 'rkunhffrzrag'
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 def get_db():
-    global database
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect('project9/sgbd.db')
+        db = g._database = sqlite3.connect('project9/sgbd.db', isolation_level=None)
+        db.row_factory = dict_factory
     return db
 
 def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    get_db().commit()
-    rv = [dict((cur.description[idx][0], value)
-               for idx, value in enumerate(row)) for row in cur.fetchall()]
-    return (rv[0] if rv else None) if one else rv
+    return get_db().execute(query, args)
 
 @app.teardown_appcontext
 def close_connection(exception):
--- project9/actions.py
+++ project9/actions.py
@@ -1,30 +1,40 @@
 from project9 import app, query_db
-from flask import request, redirect, url_for, render_template, g, jsonify
+from flask import request, redirect, url_for, render_template, g, jsonify, flash
 import random
 
 
 @app.route("/")
 def index():
-        
+
     return render_template('index.html', test=1)
 
 @app.route("/search", methods=['POST'])
 def search():
-    
+
     pattern = request.form['pattern']
     op = ' AND ' if request.form['all'] == 'true' else ' OR '
     if not pattern:
         return jsonify(success=True)
-    
+
     words = list(map(str.lower, map(lambda x: x if '%' in x else '%' + x + '%', pattern.split(' '))))
-    
+
     questions = query_db('SELECT id, content AS text FROM questions WHERE ' + op.join(['LOWER(content) LIKE ?' for i in range(len(words))]) + ' LIMIT 10', words)
     categories = query_db('SELECT id, name AS text FROM categories WHERE ' + op.join(['LOWER(name) LIKE ?' for i in range(len(words))]) + ' LIMIT 10', words)
     cours = query_db('SELECT sigle AS id, name AS text FROM cours WHERE ' + op.join(['LOWER(name) LIKE ?' for i in range(len(words))]) + ' LIMIT 10', words)
     parties_cours = query_db('SELECT id, name AS text FROM partie_cours WHERE ' + op.join(['LOWER(name) LIKE ?' for i in range(len(words))]) + ' LIMIT 10', words)
-    
+
     return jsonify(success=True, questions=questions, categories=categories, cours=cours, partie_cours=parties_cours)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.form:
+        cur = query_db('select * from users where email = ? and password = ?',
+                (request.form['email'], request.form['password']))
+        if cur.fetchone():
+            return redirect('home.html')
+        else:
+            flash('Login attempt fail!')
+    return render_template('login.html')
 
 @app.route('/prof/', methods=['GET', 'POST'])
 def prof():
